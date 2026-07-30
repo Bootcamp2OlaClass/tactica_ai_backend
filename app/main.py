f075c5a2452f068270f7fc525f15cadbc71e9e48
NhiNguyen <nyn5130@psu.edu>
2026-07-30 19:26:56 -0400

    feat/ta-44: Implement task API (#28)
    Co-authored-by: Nhi Nguyen <yn20102005@gmail.com>

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ from app.routers import auth
 from app.routers import courses
 from app.routers import test
 from app.routers import rbac_test
+from app.routers import task
 from app.routers import semester
 
 
@@ -49,6 +50,7 @@ app.include_router(auth.router)
 app.include_router(courses.router)
 app.include_router(test.router)
 app.include_router(rbac_test.router)
+app.include_router(task.router)
 app.include_router(semester.router)
 
 
